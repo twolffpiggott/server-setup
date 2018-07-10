0c36a9220947a2ff260dc568786e6d0620993e3c
twolffpiggott <twolffpiggott@gmail.com>
2018-07-10 23:09:49 +0000

    added tensorflow 1.7 setup scripts and requirements

---
+++ cudnn_install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo dpkg -i libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb
---
+++ set_path_vars.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
