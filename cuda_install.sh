ff4e86acdd5679a1ddc8560e578a63f9024865a6
twolffpiggott <twolffpiggott@gmail.com>
2018-07-12 17:07:02 +0200

    fetch cuda keys

--- cuda_install.sh
+++ cuda_install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt-get update
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-get update
